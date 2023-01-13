902c405d021f2bf8aaac48e83d8a896761b73d31
Alex <alexlarios49@gmail.com>
2023-01-13 17:03:34 +0100

    model of header & footer

---
+++ model.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>FORMS ABP</title>
+</head>
+<header>
+    <p>BENVINGUT</p>
+</header>
+<body>
+
+</body>
+<footer>
+    <p name="producer">ABP Web Technology</p>
+</footer>
+</html>
