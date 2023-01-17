ff4d26f0bac14abb48f8e590f60c3962e4876607
Alex <alexlarios49@gmail.com>
2023-01-17 09:52:55 +0100

    common header

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
-<!DOCTYPE html>
+<?php
+$_GET['titlePage'] = 'LandingPage';
+$_GET['bodyID'] = 'landing';
+$_GET['bodyClass'] = 'landing';
+?><!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<?php include 'header.php';?>
-<body>
-    
+    <?php include 'header.php';?>
+    <?php include 'footer.php';?>
 </body>
-<?php include 'footer.php';?>
 </html>
--- teacher.php
+++ teacher.php
@@ -1,152 +1,145 @@
 <?php
- session_start();
- include 'utilities.php';
- $user = logUser();
-?><!DOCTYPE html>
+session_start();
+$_GET['titlePage'] = 'Teacher';
+$_GET['bodyID'] = 'teacher';
+$_GET['bodyClass'] = '';
+include 'utilities.php';
+$user = logUser();
+?>
+<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-    <link rel="stylesheet" href="styles.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-    <script src="https://kit.fontawesome.com/277f72a273.js" crossorigin="anonymous"></script>
-</head>
-<body id='teacher'>
-<script src="./utilities.js"></script>
-
-<?php include 'header.php';?>
-    <div id='divTeacher'>
-        <?php if($user['role'] == 1){   ?>
-            <br>
-            <div class = "messageBox"></div>
-            <div id="divButtons">
-                <a class="button" id='createQuestion'>CREAR PREGUNTA</a>
-                <a class="button">CREAR ENQUESTA</a>
-                <a class="button" id='questionList'>LLISTAT PREGUNTES</a>
-                <a class="button" id='pollList'>LLISTAT ENQUESTA</a>
-            </div>
-            <?php }?>
-            <div id="divDinamic">
-                <?php 
-            $startSession = connToDB()->prepare("SELECT * FROM `poll`;");
-            $startSession->execute();
-            echo '<div id="polls">';
-            foreach($startSession as $poll){
-                echo "\n".'<p id='.$poll['ID'].'>'.$poll['title'].'</p>';
-            }
-            echo "\n".'</div>';
-
-            $startSession = connToDB()->prepare("SELECT * FROM `question`;");
-            $startSession->execute();
-            echo '<div id="questions" hidden>';
-            foreach($startSession as $question){
-                echo "\n".'<p id='.$question['ID'].'>'.$question['question'].'</p>';
-            }
-            echo "\n".'</div>';
-
-            echo '<form action="checkoutForms.php" method="POST" id="newQuestion" hidden>';
-                echo "<input name='questionTitle' type='text' id='questionTitle'><br>";
-                echo '<select name="typeQuestion" id="typeSelect">';
-                    echo "\n".'<option id="0" selected></option><br>';
-                    $startSession = connToDB()->prepare("SELECT * FROM `type_of_question`;");
-                    $startSession->execute();
-                    foreach($startSession as $type_option){
-                        echo "\n".'<option id='.$type_option['ID'].' value='.$type_option['ID'].'>'.$type_option['name'].'</option>';
-                    }
-                echo "\n".'</select><br><br>';
-                echo '<textarea id="taQuestion" rows="5" cols="33" disabled></textarea>';
-                echo '<div id="radioGroup">';
-                    $startSession = connToDB()->prepare("SELECT * FROM `option` WHERE ID <= 5;");
-                    $startSession->execute();
-                    $_SESSION['arrayOptions'] = [];
-                    foreach($startSession as $opinion){
-                        echo "\n".'<a><input type="radio" id="'.$opinion['ID'].'" name="score" value="'.$opinion['ID'].'" disabled><label for="'.$opinion['ID'].'">'.$opinion['answer'].'</label></a>';
-                        array_push($_SESSION['arrayOptions'], $opinion['ID']);
-                    }
-                echo "\n".'</div>';
-                echo '<br><input id="saveQuestion" type="submit" value="Guardar"/>';
-                echo '<input id="clearForm" type="reset" value="Cancelar"/>';
-            echo "\n".'</form>';
-        ?>
+<?php include 'header.php'; ?>
+<div id='divTeacher'>
+    <?php if ($user['role'] == 1) {   ?>
+        <br>
+        <div class="messageBox"></div>
+        <div id="divButtons">
+            <a class="button" id='createQuestion'>CREAR PREGUNTA</a>
+            <a class="button">CREAR ENQUESTA</a>
+            <a class="button" id='questionList'>LLISTAT PREGUNTES</a>
+            <a class="button" id='pollList'>LLISTAT ENQUESTA</a>
         </div>
+    <?php } ?>
+    <div id="divDinamic">
+        <?php
+        $startSession = connToDB()->prepare("SELECT * FROM `poll`;");
+        $startSession->execute();
+        echo '<div id="polls">';
+        foreach ($startSession as $poll) {
+            echo "\n" . '<p id=' . $poll['ID'] . '>' . $poll['title'] . '</p>';
+        }
+        echo "\n" . '</div>';
+
+        $startSession = connToDB()->prepare("SELECT * FROM `question`;");
+        $startSession->execute();
+        echo '<div id="questions" hidden>';
+        foreach ($startSession as $question) {
+            echo "\n" . '<p id=' . $question['ID'] . '>' . $question['question'] . '</p>';
+        }
+        echo "\n" . '</div>';
+
+        echo '<form action="checkoutForms.php" method="POST" id="newQuestion" hidden>';
+        echo "<input name='questionTitle' type='text' id='questionTitle'><br>";
+        echo '<select name="typeQuestion" id="typeSelect">';
+        echo "\n" . '<option id="0" selected></option><br>';
+        $startSession = connToDB()->prepare("SELECT * FROM `type_of_question`;");
+        $startSession->execute();
+        foreach ($startSession as $type_option) {
+            echo "\n" . '<option id=' . $type_option['ID'] . ' value=' . $type_option['ID'] . '>' . $type_option['name'] . '</option>';
+        }
+        echo "\n" . '</select><br><br>';
+        echo '<textarea id="taQuestion" rows="5" cols="33" disabled></textarea>';
+        echo '<div id="radioGroup">';
+        $startSession = connToDB()->prepare("SELECT * FROM `option` WHERE ID <= 5;");
+        $startSession->execute();
+        $_SESSION['arrayOptions'] = [];
+        foreach ($startSession as $opinion) {
+            echo "\n" . '<a><input type="radio" id="' . $opinion['ID'] . '" name="score" value="' . $opinion['ID'] . '" disabled><label for="' . $opinion['ID'] . '">' . $opinion['answer'] . '</label></a>';
+            array_push($_SESSION['arrayOptions'], $opinion['ID']);
+        }
+        echo "\n" . '</div>';
+        echo '<br><input id="saveQuestion" type="submit" value="Guardar"/>';
+        echo '<input id="clearForm" type="reset" value="Cancelar"/>';
+        echo "\n" . '</form>';
+        ?>
     </div>
-    <?php include 'footer.php';?>
+</div>
+<?php include 'footer.php'; ?>
 </body>
 <script>
     $('#radioGroup').hide();
     $("#taQuestion").hide();
     $("#saveQuestion").hide();
 
-    function changeColor(button_id){
-        $('.button').css("background-color","#62929E");
-        $(button_id).css("background-color","blue");
+    function changeColor(button_id) {
+        $('.button').css("background-color", "#62929E");
+        $(button_id).css("background-color", "blue");
     }
 
     changeColor("#pollList");
-        $(document).ready(function(){
-            $("#questionList").click(function(){
-                $("#polls").hide();
-                $("#newQuestion").hide();
-                $("#questions").show();
-                changeColor('#questionList');
-            });
+    $(document).ready(function() {
+        $("#questionList").click(function() {
+            $("#polls").hide();
+            $("#newQuestion").hide();
+            $("#questions").show();
+            changeColor('#questionList');
+        });
 
-            $("#pollList").click(function(){
-                $("#questions").hide();
-                $("#newQuestion").hide();
-                $("#polls").show();
-                changeColor('#pollList');
-            });
+        $("#pollList").click(function() {
+            $("#questions").hide();
+            $("#newQuestion").hide();
+            $("#polls").show();
+            changeColor('#pollList');
+        });
 
-            $("#createQuestion").click(function(){
-                $("#polls").hide();
-                $("#questions").hide();
-                $("#newQuestion").show();
-                changeColor('#createQuestion');
-            });;
+        $("#createQuestion").click(function() {
+            $("#polls").hide();
+            $("#questions").hide();
+            $("#newQuestion").show();
+            changeColor('#createQuestion');
+        });;
 
-            $('#typeSelect').on('change', function() {
-                if($("#typeSelect option:selected").attr("id") == 2){
-                    $('#radioGroup').hide();
-                    $("#taQuestion").show();
-                }else if($("#typeSelect option:selected").attr("id") == 1){
-                    $("#taQuestion").hide();
-                    $('#radioGroup').show();
-                }else if($("#typeSelect option:selected").attr("id") == 0){
-                    $('#radioGroup').hide();
-                    $("#taQuestion").hide();
-                }
-
-                if(document.getElementById("questionTitle").value.length && $("#typeSelect option:selected").attr("id") != 0){
-                    $("#saveQuestion").show();
-                }else{
-                    $("#saveQuestion").hide();
-                }
-            });
-
-            $('#questionTitle').on('input',function(e){
-                if(/^\s/.test($('#questionTitle').val())){
-                   $('#questionTitle').val('');
-                }
-                if(document.getElementById("questionTitle").value.length && $("#typeSelect option:selected").attr("id") != 0){
-                    $("#saveQuestion").show();
-                }else{
-                    $("#saveQuestion").hide();
-                }
-            });
-        });     
-    </script>
-    <?php
-        if (isset($_SESSION['errors']) && (!empty($_SESSION["errors"]))) {
-            foreach ($_SESSION['errors'] as $key => $value) {
-                echo "
-                <script>
-                    ".$value."
-                </script>";
+        $('#typeSelect').on('change', function() {
+            if ($("#typeSelect option:selected").attr("id") == 2) {
+                $('#radioGroup').hide();
+                $("#taQuestion").show();
+            } else if ($("#typeSelect option:selected").attr("id") == 1) {
+                $("#taQuestion").hide();
+                $('#radioGroup').show();
+            } else if ($("#typeSelect option:selected").attr("id") == 0) {
+                $('#radioGroup').hide();
+                $("#taQuestion").hide();
             }
-            $_SESSION['errors'] = [];
-        }
-    ?>
+
+            if (document.getElementById("questionTitle").value.length && $("#typeSelect option:selected").attr("id") != 0) {
+                $("#saveQuestion").show();
+            } else {
+                $("#saveQuestion").hide();
+            }
+        });
+
+        $('#questionTitle').on('input', function(e) {
+            if (/^\s/.test($('#questionTitle').val())) {
+                $('#questionTitle').val('');
+            }
+            if (document.getElementById("questionTitle").value.length && $("#typeSelect option:selected").attr("id") != 0) {
+                $("#saveQuestion").show();
+            } else {
+                $("#saveQuestion").hide();
+            }
+        });
+    });
+</script>
+<?php
+if (isset($_SESSION['errors']) && (!empty($_SESSION["errors"]))) {
+    foreach ($_SESSION['errors'] as $key => $value) {
+        echo "
+                <script>
+                    " . $value . "
+                </script>";
+    }
+    $_SESSION['errors'] = [];
+}
+?>
+
 </html>
