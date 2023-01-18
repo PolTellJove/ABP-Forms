ac26c3d352ced4e73fbd35f53f76163b178dc717
AzureAD\BrahianMonsalveMuñoz <bmonsalve@5skill.com>
2023-01-18 11:36:38 +0100

    Deleted Iframe

--- footer.php
+++ footer.php
@@ -22,8 +22,7 @@
                 On som?
             </h5>
             <p>Carrer Bonavista, 70, 08940 Cornellà de Llobregat, Barcelona</p>
-            <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2994.851585782622!2d2.074140350722113!3d41.35557950582809!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4995cdcf4979d%3A0xf7993c3566e6151f!2sInstituto%20Esteve%20Terradas%20i%20Illa!5e0!3m2!1ses!2ses!4v1673999259210!5m2!1ses!2ses" width="300" height="200"allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
         </div>
     </div>    
-    <p class="pCopyright"><i> &copy;ABP Web Technology 2023 - Tots els drets reservats</i></p>
+    <p class="pCopyright"><i> &copy; ABP Web Technology 2023 - Tots els drets reservats</i></p>
 </footer>
